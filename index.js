54a600968e16eb012643c7128a0f822b6e0afad2
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-04-27 16:39:54 +0600

    add javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+let color = "salmon";
+
+switch(color) {
+
+    case "red":
+        case "green":
+            case "yellow":
+                case "blue":
+                    console.log("you have selected RGB color");
+                    break;
+                    case "black":
+                        case "salmon":
+                            case "cadteblue":
+                                case "sky":
+                                    console.log("you have selected MKCY color");
+                                    break;
+
+}
